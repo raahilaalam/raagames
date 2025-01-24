3e6e71adf91016b74fe46b911996de55d6396adf
raahilaalam <156442056+raahilaalam@users.noreply.github.com>
2025-01-24 10:07:54 -0500

    Create script.js

---
+++ projects/DTI/script.js
@@ -0,0 +1,4 @@
+document.getElementById('start-game').addEventListener('click', function() {
+    const gameContainer = document.getElementById('game-container');
+    gameContainer.innerHTML = '<p>Game started! Time to create stylish outfits and impress everyone!</p>';
+});
