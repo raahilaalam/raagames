33c4f85ea03410c04452740926827cae47aa2c60
raahilaalam <156442056+raahilaalam@users.noreply.github.com>
2025-01-24 10:56:24 -0500

    Delete projects/DTI/script.js

--- projects/DTI/script.js
+++
@@ -1,8 +0,0 @@
-function selectCharacter(base) {
-    document.getElementById('base').src = base;
-    document.getElementById('outfit').src = '';
-}
-
-function dressCharacter(outfit) {
-    document.getElementById('outfit').src = outfit;
-}
